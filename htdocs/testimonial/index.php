1ce6abd3428b786f999ab8af65bd2ce4aac50979
theochino <theo@Theos-MacBook-Air.local>
2019-12-17 22:18:50 +1100

    Added video testimony

--- htdocs/testimonial/index.php
+++ htdocs/testimonial/index.php
@@ -118,8 +118,10 @@
 				</div>                           
 
 				<P>
-					
 
+
+<iframe width="560" height="315" src="https://www.youtube.com/embed/Qga3Ut1Yuvw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+<iframe width="560" height="315" src="https://www.youtube.com/embed/02uwl_6053M" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 				</P>
 
 
